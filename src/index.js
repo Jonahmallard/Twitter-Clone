ac8e841d4b71d4f2325813071d85164e55b1e0b1
Jonah Mallard <benjaminjmallard@gmail.com>
2021-02-02 17:10:01 -0600

    Update App.js, Index.js, and add react-router/redux/thunk

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
