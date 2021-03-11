4d207cc18a084a2c020b89a485a6695f34a031eb
Jonah Mallard <benjaminjmallard@gmail.com>
2021-03-11 13:24:35 -0600

    fix indentation

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -7,32 +7,31 @@ export default function postReducer(state = { posts: [] }, action) {
         case 'ADD_COMMENT': 
             let posts = state.posts.map(post => {
                 if (post.id === action.payload.id) {
-                return action.payload 
-            } else {
-                return post
+                    return action.payload 
+                } else {
+                    return post
             }
         })
             return {...state, posts: posts}
         case 'DELETE_COMMENT': 
             let post = state.posts.map(post => {
                 if (post.id === action.payload.id) {
-                return action.payload 
-            } else {
-                return post
+                    return action.payload 
+                } else {
+                    return post
             }
         })
             return {...state, posts: post}
             case 'EDIT_POST':
                 let updatedPost = state.posts.map(post => {
                     if (post.id === action.payload.id) {
-                    return action.payload 
-                } else {
-                    return post
+                        return action.payload 
+                    } else {
+                        return post
                 }
             })
                 return {...state, posts: updatedPost}
-
-    default:
-        return state;
-    }
+        default:
+            return state;
+        }
 }
