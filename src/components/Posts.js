bf5cf4e2220ecb41ece1f55610da42c64ca90dd2
Jonah <benjaminjmallard@gmail.com>
2021-06-30 21:27:24 -0500

    update styling

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,17 +2,12 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 const Posts = (props) => {
-    console.log(props.posts);
-    const [showSortedVersion, setShowSortedVersion] = useState(false);
-    const sortPosts = [...props.posts].sort((a, b) => b.likes - a.likes);
-    console.log(sortPosts);
     return (
         <div>
             <br/>
             <h1>Tweeties</h1>
-            <button onClick={() => setShowSortedVersion(!showSortedVersion)}>Sort</button>
             <br/>
-            {(showSortedVersion ? sortPosts : props.posts).map(post => 
+            {props.posts.map(post => 
             <div key={post.id} className="card">
                 <div className="card-body">
                     <h3 className="card-title">User: <strong>{post.username}</strong></h3>
