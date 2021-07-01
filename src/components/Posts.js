7b5565fc00ace887d465671f57a3be80c7851001
Jonah <benjaminjmallard@gmail.com>
2021-06-30 21:14:41 -0500

    sorting posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,18 +2,17 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 const Posts = (props) => {
-    // const [sortedPosts, setSortedPosts] = useState(false);
-    // const sortPosts = props.posts.sort((a, b) => a.likes - b.likes);
-    // const unSortPosts = props.posts.sort((a, b) => b.likes - a.likes);
-    // console.log(sortPosts);
-    // console.log(props.posts);
+    console.log(props.posts);
+    const [showSortedVersion, setShowSortedVersion] = useState(false);
+    const sortPosts = [...props.posts].sort((a, b) => b.likes - a.likes);
+    console.log(sortPosts);
     return (
         <div>
             <br/>
             <h1>Tweeties</h1>
-            {/* <button onClick={sortedPosts ? setSortedPosts : sortPosts}>Sort</button> */}
+            <button onClick={() => setShowSortedVersion(!showSortedVersion)}>Sort</button>
             <br/>
-            {props.posts.map(post => 
+            {(showSortedVersion ? sortPosts : props.posts).map(post => 
             <div key={post.id} className="card">
                 <div className="card-body">
                     <h3 className="card-title">User: <strong>{post.username}</strong></h3>
